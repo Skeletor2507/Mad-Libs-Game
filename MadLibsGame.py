0daf6fdb8e0ef442c56f0d7e3113a88eba900d2c
Skeletor2507 <79785216+Skeletor2507@users.noreply.github.com>
2021-03-10 21:39:23 +0000

    Add files via upload

---
+++ MadLibsGame.py
@@ -0,0 +1,33 @@
+#mad libs game: a superhero movie
+
+normal_name = input("Enter a normal name for our hero, e.g. Peter Parker: ")
+profession = input("Enter a profession: ")
+country = input("Enter a country: ")
+hero_name = input("Enter an alter-ego superhero name for our hero, e.g. Spider-Man: ")
+colour = input("Enter a colour: ")
+adjective = input("Enter an adjective to describe something, e.g. huge, round, sweet, fat, shiny... : ")
+girlfriend = input("Enter the name of our hero's girlfriend, e.g. Mary-Jane Watson: ")
+dundee_pub = input("Enter the name of a Dundee pub, e.g. The Counting House: ")
+friend = input("Enter the name of our hero's most crazy friend, e.g. Harry Osbourne: ")
+body_feature = input("Enter the name of a distinguishable, and preferably non-flattering, body feature: ")
+animal = input("Enter the name of an animal: ")
+city = input("Enter the name of any city in the world: ")
+villain = input("Enter the name of our hero's nemesis, e.g. Green Goblin: ")
+act_of_violence = input("Enter a method of violence, e.g. punch, kick, headbutt: ")
+body_part = input("Enter a body part: ")
+down_and_out_actor = input("Enter the name of an ageing, over-the-hill actor: ")
+
+
+print("Meet our hero, " + normal_name + ", a super-intelligent " + profession + " by day, brave crime-fighting superhero by night.\n")
+print("A cataclysmic run-in with the military of " + country + " leads him to create his alter-ego, " + hero_name + ", a " + colour + " " + adjective + "\n beast capable of total annihilation.\n")
+print(hero_name + " bravely battles against the military alongside his fearless girlfriend and childhood sweetheart, " + girlfriend + ", \nwhom he had met many years prior in a dark alleyway near to the back passage of " + dundee_pub + ".")
+print("Eventually, " + hero_name + " is devastated to learn the news that his long-time best friend, " + friend + ", distinguished by \n his rather unfortunate " +  body_feature + ", is inexplicably trying to turn his pet " + animal + " into a bionic weapon.")
+print("This leads to a climatic final battle in downtown " + city + " where an evil dark overlord by the name of " + villain + " stands in our hero's path.")
+print("Eventually, the enemy is subdued by " + act_of_violence + "ing" + " him right square in the " + body_part + ".")
+print("\nAfter the final credits roll, " + down_and_out_actor + " rides in on the back of Viscerion from HBO's Game Of Throne's, teasing a sequel.")
+
+
+
+
+
+
